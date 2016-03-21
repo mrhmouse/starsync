18df3f6ea9485a20fb0cade04c9e5b92b1731b0c
Jordan Brown <mrhmouse@gmail.com>
2016-03-21 10:34:30 -0400

    Initial commit

---
+++ starsync.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/zsh
+
+USER=
+STARDIR=
+
+main() {
+    prompt-for-username
+    make-star-directory
+    fetch-all-stars
+}
+
+prompt-for-username() {
+    USER="$(whoami)"
+    if prompt "Using $USER as your username; is this OK?" ; then
+        return
+    fi
+
+    input USER "Please enter your username"
+}
+
+make-star-directory() {
+    STARDIR="${USER}'s stars"
+    mkdir -p "$STARDIR"
+}
+
+fetch-all-stars() {
+    curl https://api.github.com/users/mrhmouse/starred | jq -r '.[]|@sh "\(.git_url)", "\(.author)", "\(.name)"' |\
+        while read -r URL AUTHOR NAME ; do
+            clone-or-pull "$URL" "$AUTHOR" "$NAME"
+        done
+}
+
+prompt() {
+    echo -n "$@ (y/n) "
+    read -r ANSWER
+    if test 0 -ne $? ; then
+        prompt "$@"
+        return $?
+    fi
+
+    ANSWER="${ANSWER[1]}"
+    if test "$ANSWER" = y -o Y = "$ANSWER" ; then
+        return 0
+    elif test "$ANSWER" = n -o N = "$ANSWER" ; then
+        return 1
+    fi
+
+    prompt "$@"
+}
+
+input() {
+    local VAR="$1"
+    shift
+    echo -n "${@}: "
+    read -r "$VAR"
+    if test 0 -ne $? ; then
+        input "$VAR" "$@"
+        return
+    fi
+}
+
+msg() {
+    echo "[$@]"
+}
+
+clone-or-pull() {
+    local URL="$1"
+    local AUTHOR="$2"
+    local NAME="$3"
+    if test -d "$AUTHOR/$NAME" ; then
+        msg "Updating $NAME..."
+        cd "$AUTHOR/$NAME"
+        git pull --ff-only
+    else
+        msg "Cloning $NAME..."
+        mkdir -p "$AUTHOR"
+        cd "$AUTHOR"
+        git clone "$URL"
+    fi
+}
+
+main
